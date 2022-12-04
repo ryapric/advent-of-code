20ee8393ac0e598f2330b743ac796983ae1b203f
Ryan J. Price <ryapric@gmail.com>
2022-12-03 19:05:49 -0600

    2022.3.2 solved

--- year2022/day03/main.R
+++ year2022/day03/main.R
@@ -14,3 +14,20 @@ overlaps <- lapply(compartments, function(x) intersect(unlist(strsplit(x[1], "")
 overlap_priorities <- lapply(overlaps, function(x) which(x == c(letters, LETTERS)))
 # ... and sums those priority numbers
 print(paste("Sum of item priorities:", sum(sapply(overlap_priorities, function(x) x))))
+
+
+# Part 2 makes this a bit wonkier, and we might actually need a real loop
+sum_priority <- 0
+for (i in seq(1, length(compartments), 3)) {
+  # Un-split the compartments first
+  rucksacks <- list(
+    one = unlist(strsplit(paste0(compartments[[i]][1], compartments[[i]][2]), "")),
+    two = unlist(strsplit(paste0(compartments[[i+1]][1], compartments[[i+1]][2]), "")),
+    three = unlist(strsplit(paste0(compartments[[i+2]][1], compartments[[i+2]][2]), ""))
+  )
+  # Get the badge's letter & add its priority
+  badge <- Reduce(intersect, rucksacks)
+  sum_priority <- sum_priority + which(badge == c(letters, LETTERS))
+}
+
+print(paste("Sum of item priorities (pt2):", sum_priority))
