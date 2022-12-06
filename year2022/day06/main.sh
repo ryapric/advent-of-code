a666e6f742bbd094299fdcaa59a15490fc97e986
Ryan J. Price <ryapric@gmail.com>
2022-12-06 14:29:55 -0600

    2022.6.2 solved

--- year2022/day06/main.sh
+++ year2022/day06/main.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-read -ra input <<< "$(sed 's/./& /g' ./input.txt)"
-desired_nonrepeating=4
+input="${1}"
 
-pos='-1' # Start at -1 so we can increment straight to index 0
-for _ in "${input[@]}"; do
-  pos="$((pos + 1))"
-  if [[ "${pos}" -lt 3 ]]; then continue; fi
+read -ra input <<< "$(sed 's/./& /g' ./${input})"
+start_of_run_length='start_of_run_length' # dummy var for nested-var interpolation
+start_of_run_length_packet=4
+start_of_run_length_message=14
 
-  # Buffer loop to put chars on their own lines so they're uniq-able
-  for char in $(echo "${input[@]:$((pos-3)):${desired_nonrepeating}}"); do
-    echo "${char}"
-  done > /tmp/uniq-able
-  uniqe_chars="$(sort /tmp/uniq-able | uniq  | wc -l)"
-  if [[ "${uniqe_chars}" -eq "${desired_nonrepeating}" ]] ; then
-    printf "This many chars processed until a nonrepeating sequence of %s found: %s\n" "${desired_nonrepeating}" "$((pos + 1))"
-    break
-  fi
+for signature in packet message ; do
+  pos='-1' # Start at -1 so we can increment straight to index 0
+  for _ in "${input[@]}"; do
+    pos="$((pos + 1))"
+    if [[ "${pos}" -lt $((${start_of_run_length}_${signature} - 1)) ]]; then continue; fi
+
+    # Buffer loop to put chars on their own lines so they're uniq-able
+    # this is a nasty artihmetic expansion lol
+    for char in $(echo "${input[@]:$((pos-(${start_of_run_length}_${signature}-1))):${start_of_run_length}_${signature}}"); do
+      echo "${char}"
+    done > /tmp/uniq-able
+
+    uniqe_chars="$(sort /tmp/uniq-able | uniq  | wc -l)"
+    if [[ "${uniqe_chars}" -eq "${start_of_run_length}_${signature}" ]] ; then
+      printf 'This many chars processed until start-of-%s run length of %s found: %s\n' "${signature}" "$((${start_of_run_length}_${signature}))" "$((pos + 1))"
+      break
+    fi
+  done
 done
